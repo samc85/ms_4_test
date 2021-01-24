5ee4d415af8a648ccdbdfc55e08e8aac487b841a
Sam Croyle <sam300385@hotmail.com>
2021-01-24 17:03:37 +0000

    Navbar collaps draft look at and more rows for products added for sample

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+# Import static if not imported
+from django.conf.urls.static import static
 
 app_name = 'products'
 
 urlpatterns = [
     path('', views.products, name='products'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
